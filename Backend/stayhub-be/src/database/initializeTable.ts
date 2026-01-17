998e1a73ed11f18ebcc2bd72f80587021413d6b8
minorin2847 <minorin2847@gmail.com>
2026-01-17 15:22:27 +0700

    [BACKEND] tables: initialize users table and initial admin account

--- Backend/stayhub-be/src/database/initializeTable.ts
+++ Backend/stayhub-be/src/database/initializeTable.ts
@@ -1,15 +1,26 @@
 import { DatabaseError } from '@/types/DatabaseError.js';
+import crypto from 'node:crypto';
 import db from './db.js';
 
 const tables = {
     hello: `
-            CREATE TABLE IF NOT EXISTS hello (
-                id      INT             PRIMARY KEY     GENERATED ALWAYS AS IDENTITY,
-                name    VARCHAR(100)    NOT NULL
-            );
+        CREATE TABLE IF NOT EXISTS hello (
+            id      INT             PRIMARY KEY     GENERATED ALWAYS AS IDENTITY,
+            name    VARCHAR(100)    NOT NULL
+        );
     `,
+    user: `
+        CREATE TABLE IF NOT EXISTS users (
+            id          INT             PRIMARY KEY     GENERATED ALWAYS AS IDENTITY,
+            username    VARCHAR(100)    UNIQUE NOT NULL,
+            salt        BYTEA     NOT NULL,
+            hash        BYTEA        NOT NULL,
+            roles       VARCHAR(100)[]  NOT NULL    DEFAULT '{ROLE_USER}'
+        );
+    `
 }
 export default async function initializeTable() {
+    // Initialize tables
     for (const [name, table] of Object.entries(tables)) {
         try {
         await db.none(table);
@@ -20,4 +31,14 @@ export default async function initializeTable() {
             } else throw new DatabaseError(`An unknown error occured when creating table ${name}`);
         }
     }
+    // Initialize admin account
+    const salt = Buffer.from('k:UK�\b��r�*"`��F');
+    const password = "123456"
+    const hash = crypto.pbkdf2(password, salt, 310000, 32, 'sha256', (err, hashed) => {
+        if (err) throw new DatabaseError(err.message);
+        db.oneOrNone("INSERT INTO users(username, salt, hash) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING RETURNING id, username;", ["admin", salt, hashed]).then(() => {
+            console.log(`Initialized admin account!`);
+        })
+
+    });
 }
