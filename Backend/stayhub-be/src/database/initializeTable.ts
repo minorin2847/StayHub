dcab035510654153e0efd0876f85bb62f2db95ce
minorin2847 <minorin2847@gmail.com>
2026-01-23 01:39:40 +0700

    [BACKEND] init: add new table and admin acount init and session serialize

--- Backend/stayhub-be/src/database/initializeTable.ts
+++ Backend/stayhub-be/src/database/initializeTable.ts
@@ -1,6 +1,6 @@
-import { DatabaseError } from '@/types/DatabaseError.js';
 import crypto from 'node:crypto';
 import db from './db.js';
+<<<<<<< Updated upstream
 
 const tables = {
     hello: `
@@ -30,26 +30,79 @@ WITH (OIDS=FALSE);
 CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "session" USING BTREE ("expire");
     `
 }
+=======
+import toRoleString from '@/utils/toRoleString.js';
+import pgPromise from 'pg-promise';
+
+>>>>>>> Stashed changes
 export default async function initializeTable() {
     // Initialize tables
-    for (const [name, table] of Object.entries(tables)) {
-        try {
-        await db.multi(table);
-        console.log(`Table ${name} initialized!`);
-        } catch (err) {
-            if (err instanceof Error) {
-                throw new DatabaseError(`An error occured when creating table ${name}: ${err.message}`);
-            } else throw new DatabaseError(`An unknown error occured when creating table ${name}`);
-        }
+    try {
+        const pgp = pgPromise();
+        const { QueryFile } = pgp;
+        const file = new QueryFile("./database/StayHub.sql");
+        await db.multi(file);
+        console.log(`All tables initialized!`);
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error(`An error occured when creating tables: ${err}`);
+        } else console.error(`An unknown error occured when creating tables`);
     }
     // Initialize admin account
     const salt = Buffer.from('k:UK�\b��r�*"`��F');
     const password = "123456"
     const hash = crypto.pbkdf2(password, salt, 310000, 32, 'sha256', (err, hashed) => {
+<<<<<<< Updated upstream
         if (err) throw new DatabaseError(err.message);
         db.oneOrNone("INSERT INTO users(username, salt, hash, roles) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id, username;", ["admin", salt, hashed, "{" + ["ROLE_USER", "ROLE_ADMIN"].join(",") + "}"]).then(() => {
             console.log(`Initialized admin account!`);
         })
+=======
+        if (err) throw new Error(err.message);
+        db.task('admin-creation', async t => {
+            let user = await db.oneOrNone("INSERT INTO accounts(username, salt, hash, email) \
+            VALUES ($(username), $(salt), $(hash), $(email)) \
+            ON CONFLICT DO NOTHING \
+            RETURNING id, username, email;", 
+            {
+                username: 'admin',
+                salt: salt,
+                hash: hashed,
+                email: 'admin@stayhub.com'
+            })
+            if (!user) {
+                user = await db.one("SELECT id FROM accounts WHERE username='admin'");
+            }
+            const id = user.id;
+            console.log(`Initialized admin account with id ${id}!`);
+            let employee = await db.oneOrNone("INSERT INTO employees(accountID, firstName, lastName, salary) \
+            VALUES ($(id), $(firstName), $(lastName), $(salary)) \
+            ON CONFLICT DO NOTHING \
+            RETURNING id, accountID, firstName, lastName, salary;",{
+                id: id,
+                firstName: 'John',
+                lastName: 'Admin',
+                salary: 36363636
+            });
+            if (!employee) {
+                employee = await db.one("SELECT id from employees where accountID=$1", [id]);
+            }
+            const employeeID = employee.id;
+            console.log(`Initialized admin employee account with employeeID ${employeeID}!`);
+            let roles = await db.oneOrNone("INSERT INTO roles(employeeID, role) \
+                VALUES ($(employeeID), $(role)) \
+                ON CONFLICT DO NOTHING \
+                RETURNING employeeID, role", {
+                    employeeID: employeeID,
+                    role: 'ROLE_ADMIN'
+                });
+            if (!roles) {
+                roles = await db.one("SELECT * FROM roles WHERE employeeID=$1", [employeeID]);
+            }
+            console.log(`Initialized admin role for employee ${roles.employeeid} as ${roles.role}!`);
+            return roles;
+        }).catch(err => console.error(`An error occured while creating admin account: ${err}`));
+>>>>>>> Stashed changes
 
     });
 }
