827c53cba6f4ed424ff114cb0b436c1214351ce7
minorin2847 <minorin2847@gmail.com>
2026-01-28 11:05:50 +0700

    [FRONTEND] types: add hotel and room types

---
+++ Frontend/stayhub-fe/src/types/Room.ts
@@ -0,0 +1,13 @@
+export type Room = {
+    id: number;
+    hotelid: number;
+    description: string;
+    price: number;
+    size: number;
+    capacity: number;
+    type: string;
+    discount: number;
+    previewimages: Array<string>;
+    beds: Array<{name: string; count: number}>;
+    amenities: Array<{name: string; icon: string; category: string}>
+}
