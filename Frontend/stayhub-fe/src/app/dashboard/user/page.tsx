3d5913ffd8abf1ca2345a8c33962d57189cd69b0
minorin2847 <minorin2847@gmail.com>
2026-02-25 21:55:47 +0700

    [FRONTEND] modify employees table

--- Frontend/stayhub-fe/src/app/dashboard/user/page.tsx
+++ Frontend/stayhub-fe/src/app/dashboard/user/page.tsx
@@ -2,6 +2,8 @@
 
 import UserTable from "@/components/dashboard/UserTable";
 import { Account } from "@/types/Account";
+import { Employee, EmployeeTableData } from "@/types/Employee";
+import { Role } from "@/types/Role";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
@@ -11,7 +13,7 @@ export default function ManageUser() {
     const [query, setQuery] = useState<string>(searchParams.get('name') ?? '');
     const [start, setStart] = useState<string>(searchParams.get('start') ?? '0');
     const [end, setEnd] = useState<string>(searchParams.get('end') ?? '10');
-    const [results, setResults] = useState<Account[]>([]);
+    const [results, setResults] = useState<EmployeeTableData[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     
     useEffect(() => {
@@ -26,12 +28,12 @@ export default function ManageUser() {
                         start: start,
                         end: end
                     }).toString()
-                const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dashboard/non-employee?${params}`, {
+                const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dashboard/user?${params}`, {
                     method: "GET",
                     credentials: "include",
                 });
                 const data = await res.json();
-                setResults(data['values'] as Account[]);
+                setResults(data['values'] as EmployeeTableData[]);
             } catch (error) {
                 if (error instanceof Error && error.name !== 'AbortError') {
                     console.error("An error occured: ", error);
--- Frontend/stayhub-fe/src/components/dashboard/UserTable.tsx
+++ Frontend/stayhub-fe/src/components/dashboard/UserTable.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Account } from "@/types/Account";
+import { Employee, EmployeeTableData } from "@/types/Employee";
+
 
 type UserTableParameter = {
-    tableData: Account[]
+    tableData: EmployeeTableData[]
 }
 export default function UserTable(params: UserTableParameter) {
     return (
@@ -12,15 +14,23 @@ export default function UserTable(params: UserTableParameter) {
                 <tr>
                     <th className="h-fit py-[10px] border bg-gray-100">ID</th>
                     <th className="border bg-gray-100">Username</th>
+                    <th className="border bg-gray-100">Full Name</th>
                     <th className="border bg-gray-100">Email</th>
+                    <th className="border bg-gray-100">Hotel ID</th>
+                    <th className="border bg-gray-100">Salary</th>
+                    <th className="border bg-gray-100">Roles</th>
                 </tr>
             </thead>
             <tbody>
-                {params.tableData.map(i=> (
-                    <tr>
-                        <td className="h-fit py-[10px] border text-center">{i.id}</td>
-                        <td className="border text-center">{i.username}</td>
-                        <td className="border text-center">{i.email}</td>
+                {params.tableData.map(item => (
+                    <tr key={item.id} className="group hover:bg-gray-50/50">
+                        <td className="h-fit py-[10px] border text-center">{item.id}</td>
+                        <td className="border text-center">{item.username}</td>
+                        <td className="border text-center">{item.firstname + " " + item.lastname}</td>
+                        <td className="border text-center">{item.email}</td>
+                        <td className="border text-center">{item.hotelid}</td>
+                        <td className="border text-center">{item.salary}</td>
+                        <td className="border text-center">{item.roles.map(i=>i.role).join(",")}</td>
                     </tr>
                 ))}
             </tbody>
