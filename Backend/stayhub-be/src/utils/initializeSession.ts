35d82aec72d42c52a7c30cbd2963fa51e5bd623a
minorin2847 <minorin2847@gmail.com>
2026-02-28 15:36:39 +0700

    [BACKEND] initializeSession: change session

--- Backend/stayhub-be/src/utils/initializeSession.ts
+++ Backend/stayhub-be/src/utils/initializeSession.ts
@@ -5,7 +5,7 @@ import session from 'express-session';
 import * as crypto from 'node:crypto';
 import connect_pg from 'connect-pg-simple';
 import User from '@/api/user/user.js';
-import Account from '@/api/account/account.js';
+import Employee from '@/api/employee/employee.js';
 
 
 const pgSession = connect_pg(session);
@@ -16,30 +16,60 @@ const connectionString = "postgres://" +
                             process.env.DB_PORT + "/" + 
                             process.env.DB_NAME
 
-passport.use(new local.Strategy(function verifyPassword(username, password, cb) {
-    pg.oneOrNone('SELECT * FROM accounts WHERE username = $1', [username])
-    .then(row => {
+
+function verify(row: any, password: string, cb: any) {
+    try {
         if(!row) return cb(null, false, {message: "Incorrect username or password"});
-        const user = new Account(row);
-        crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', (err, hashed) => {
+        crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', (err, hashed) => {
             if (err) return cb(err);
-            if (!crypto.timingSafeEqual(Buffer.alloc(32, user.hash), hashed)) {
+            if (!crypto.timingSafeEqual(Buffer.alloc(32, row.hash), hashed)) {
                 return cb(null, false, {message: "Incorrect username or password"});
             }
-            return cb(null, user);
+            return cb(null, row);
         })
-    })
-    .catch((err) => cb(err));
+    }
+    catch (err) {
+        cb(err)
+    };
+}
+
+passport.use("user-login", new local.Strategy(async (username , password , cb) => {
+    const row = await pg.oneOrNone("SELECT id, username, hash, salt, 'user' as type FROM users WHERE username = $1", [username]);
+    verify(row, password, cb);
+}));
+
+passport.use("employee-login", new local.Strategy(async (username , password , cb) => {
+    const row = await pg.oneOrNone("SELECT id, username, hash, salt, 'employee' as type FROM employees WHERE username = $1", [username]);
+    verify(row, password, cb);
 }))
 
-export function initializeSession() {
+export function initializeUserSession() {
     return session({
+        name: "user.sid",
         secret: process.env.SESSION_KEY || "st@yI-IubBNVTHVADu~ng",
         resave: false,
         saveUninitialized: false,
         store: new pgSession({
             conString: connectionString,
-            tableName: 'session'
+            tableName: 'user_session'
+        }),
+        cookie: {
+            secure: false,
+            httpOnly: false,
+            maxAge: 24 * 60 * 60 * 1000
+        }
+    });
+}
+
+export function initializeEmployeeSession() {
+    return session({
+        name: "employee.sid",
+        secret: process.env.SESSION_KEY || "st@yI-IubBNVTHVADu~ng",
+        resave: false,
+        saveUninitialized: false,
+        store: new pgSession({
+            conString: connectionString,
+            tableName: 'employee_session'
         }),
         cookie: {
             secure: false,
@@ -50,11 +80,25 @@ export function initializeSession() {
 }
 
 passport.serializeUser((user: any, cb) => {
-    process.nextTick(() => cb(null, Account.toDTO(user)));
+    process.nextTick(() => cb(null, {
+        username: user.username,
+        type: user.type
+    }));
 })
 
-passport.deserializeUser((user: any, cb) => {
-    process.nextTick(() => cb(null, user));
+passport.deserializeUser(async (obj: any, cb) => {
+    try {
+        console.log(obj);
+        if (obj.type == "user") {
+            const row = await pg.oneOrNone("SELECT * FROM users WHERE username=$1", [obj.username]);
+            return cb(null, row ? new User(row): false);
+        } else {
+            const row = await pg.oneOrNone("SELECT * FROM employees WHERE username=$1", [obj.username]);
+            return cb(null, row ? new Employee(row) : false);
+        }
+    } catch (err) {
+        cb(err);
+    }
 })
 
 export { passport };
