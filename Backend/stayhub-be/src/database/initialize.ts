5c3d70e380fe1a79ec460dc20891eaf822cef83b
minorin2847 <minorin2847@gmail.com>
2026-03-02 01:02:37 +0700

    [BACKEND] initialize: change admin account creation task

--- Backend/stayhub-be/src/database/initialize.ts
+++ Backend/stayhub-be/src/database/initialize.ts
@@ -30,25 +30,7 @@ export default async function initialize() {
     const hash = crypto.pbkdf2(password, salt, 310000, 32, 'sha256', (err, hashed) => {
         if (err) throw new Error(err.message);
         db.task('admin-creation', async t => {
-            let user = await db.oneOrNone("INSERT INTO employees(username, salt, hash, email, firstName, lastName, salary) \
-            VALUES ($(username), $(salt), $(hash), $(email), $(firstName), $(lastName), $(salary)) \
-            ON CONFLICT DO NOTHING \
-            RETURNING id, username, email, firstName, lastName, salary;", 
-            {
-                username: 'admin',
-                salt: salt,
-                hash: hashed,
-                email: 'admin@stayhub.com',
-                firstName: "John",
-                lastName: "Admin",
-                salary: 36363636
-            })
-            if (!user) {
-                user = await db.one("SELECT id FROM employees WHERE username='admin'");
-            }
-            const id = user.id;
-            console.log(`Initialized admin account with id ${id}!`);
-            // Initialize roles
+                        // Initialize roles
             let roles = await db.manyOrNone("INSERT INTO roles(name, tier) \
                 VALUES ('ADMINISTRATOR', 1),\
                 ('MANAGE_BRANCH', 2), \
@@ -60,18 +42,20 @@ export default async function initialize() {
                 roles = await db.many("SELECT name, tier FROM roles;");
             }
             console.log(`Initialize roles with values ${roles.map(i=>"name: " + i.name + "; tier: " + i.tier).join("\n")}!`);
-            let adminRole = await db.oneOrNone("INSERT INTO employee_roles(employeeID, role) \
-                VALUES ($(employeeID), $(role)) \
-                ON CONFLICT DO NOTHING \
-                RETURNING employeeID, role", {
-                    employeeID: id,
-                    role: 'ADMINISTRATOR'
-                });
-            if (!adminRole) {
-                adminRole = await db.one("SELECT * FROM employee_roles WHERE employeeID=$1", [id]);
-            }
-            console.log(`Initialized admin role for employee ${adminRole.employeeid} as ${adminRole.role}!`);
-            return roles;
+
+
+            const user = await db.one("SELECT * FROM create_initial_admin($(username), $(salt), $(hash), $(email), $(firstName), $(lastName), $(salary));", 
+            {
+                username: 'admin',
+                salt: salt,
+                hash: hashed,
+                email: 'admin@stayhub.com',
+                firstName: "John",
+                lastName: "Admin",
+                salary: 36363636
+            })
+            console.log(`Initialized admin account with id ${user.id}!`);
+            return;
         }).catch(err => console.error(`An error occured while creating admin account: ${err}`));
 
     });
