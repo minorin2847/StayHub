b425c756198268bacf9d965a4589a234c26127fe
minorin2847 <minorin2847@gmail.com>
2026-04-10 02:45:35 +0700

    [BACKEND] employee: add /employee/changepassword endpoint

--- Backend/stayhub-be/src/api/employee/employee.routes.ts
+++ Backend/stayhub-be/src/api/employee/employee.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createEmployee, deleteEmployee, editEmployee, getEmployee, login, logout } from "./employee.handler.js";
+import { changeEmployeePassword, createEmployee, deleteEmployee, editEmployee, getEmployee, login, logout } from "./employee.handler.js";
 import { isLoggedIn } from "../auth/auth.handler.js";
 
 const employeeRoute = Router()
@@ -13,4 +13,6 @@ employeeRoute.post("/signup", isLoggedIn, createEmployee);
 employeeRoute.get("/get/:id", isLoggedIn, getEmployee);
 employeeRoute.patch("/edit/:id", isLoggedIn, editEmployee);
 employeeRoute.delete("/delete/:id", isLoggedIn, deleteEmployee);
+
+employeeRoute.post("/changepassword", isLoggedIn, changeEmployeePassword);
 export default employeeRoute;
